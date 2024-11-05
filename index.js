200dba3a4288d154963bd5bd3d655a8c6737542b
Saurav Kumar Behera <sauravbehera.8505@gmail.com>
2024-11-05 12:01:20 +0530

    Index file updated

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+// const userRoutes = require('./routes/userRoutes');
+// const productRoutes = require('./routes/productRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
+// const couponRoutes = require('./routes/couponRoutes');
+
+
+
+
+// // Middleware to parse JSON requests
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+
+// // MongoDB connection
+// // const mongoURL = "mongodb://localhost:27017/crud1";
+// const mongoURL = "mongodb+srv://sauravrop:manager@cluster0.hbykt.mongodb.net/crud1"
+
+// mongoose
+//   .connect(mongoURL, {
+//     //    useNewUrlParser:true,
+//     //   useUnifiedTopology: true
+//   })
+//   .then(() => console.log("MongoDB Connected!"))
+//   .catch((err) => console.error(err));
+//   app.get('/', function (req, res) {
+//     res.send('Welcome');
+//   });
+
+// // Routes
+// app.use('/api/users', userRoutes);
+// app.use('/api/products', productRoutes);
+// app.use('/api/orders', orderRoutes);
+// app.use('/api/coupons', couponRoutes);
+
+// // Start the server
+// app.listen(3002, () => {
+//   console.log("Server is running on port 3002");
+// });
+
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -6,27 +48,25 @@ const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const couponRoutes = require('./routes/couponRoutes');
 
-
-
-
 // Middleware to parse JSON requests
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // MongoDB connection
-// const mongoURL = "mongodb://localhost:27017/crud1";
-const mongoURL = "mongodb+srv://sauravrop:manager@cluster0.hbykt.mongodb.net/"
+const mongoURL = "mongodb+srv://sauravrop:manager@cluster0.hbykt.mongodb.net/crud1?retryWrites=true&w=majority";
 
 mongoose
   .connect(mongoURL, {
-    //    useNewUrlParser:true,
-    //   useUnifiedTopology: true
+    ssl: true,
+    tlsAllowInvalidCertificates: true, // Use for development only, avoid in production
+    tlsAllowInvalidHostnames: true,    // Use for development only, avoid in production
   })
   .then(() => console.log("MongoDB Connected!"))
-  .catch((err) => console.error(err));
-  app.get('/', function (req, res) {
-    res.send('Welcome');
-  });
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get('/', function (req, res) {
+  res.send('Welcome');
+});
 
 // Routes
 app.use('/api/users', userRoutes);
